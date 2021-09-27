f5c917932d883afcbc8ae783f38ba4aae3343788
kjoh-dev <kevinjoh85@gmail.com>
2021-09-27 09:13:02 -0400

    Added buttons in .num-pad. Implemented grid layout on the buttons.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Buttons:
+// 1. Add 2. Subtract 3. Multiply 4. Divide 5. Equals 6. Clear 7. Period 8. Delete 9. Numbers
+// Functions/Features:
+// 1. Operate(operator, operand1, operand2)
+// 2. Round long decimals
+// 3. Handle premature = operator input
+// 4. Implement decimals
+// 5. Different colors for each operator
+// 6. Implement a delete button (backspace)
+// 7. Implement support for keyboard
